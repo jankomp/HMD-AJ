04ea23c0976a7c953b0e4ea96ef856b9ce1cb076
giTorto <giulian.trt@gmail.com>
2022-11-20 20:46:44 +0100

    Fixed data for version 3.1

--- actions.py
+++ actions.py
@@ -8,32 +8,10 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-from rasa_sdk.forms import FormAction
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
-class PizzaOrderForm(FormAction):
-
-	def name(self) -> Text:
-		"""Unique identifier of the form"""
-		return "pizza_order_form"
-
-	@staticmethod
-	def required_slots(tracker: Tracker) -> List[Text]:
-		"""A list of required slots that the form has to fill"""
-		return ["pizza_type", "pizza_amount","pizza_size"]
-
-	def submit(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict]:
-		"""Define what the form has to do after all required slots are filled"""
-		pizza_size = tracker.get_slot("pizza_size")
-		pizza_type = tracker.get_slot("pizza_type")
-		pizza_amount = tracker.get_slot("pizza_amount")
-		
-		dispatcher.utter_message(text='Okay Great. Your order is {} {} pizzas in {}. Can you confirm please'.format(pizza_amount,pizza_type,pizza_size))
-		return []
 
 class ActionChangeOrder(Action):
 	def name(self):
@@ -56,6 +34,8 @@ class ActionPizzaOrderAdd(Action):
 		pizza_size = tracker.get_slot("pizza_size")
 		pizza_type = tracker.get_slot("pizza_type")
 		pizza_amount = tracker.get_slot("pizza_amount")
+		if pizza_size is None:
+			pizza_size = "standard"
 		order_details =  str(pizza_amount + " "+pizza_type + " is of "+pizza_size )
 		old_order = tracker.get_slot("total_order")
 		return[SlotSet("total_order", [order_details]) if old_order is None else SlotSet("total_order", [old_order[0]+' and '+order_details])]
